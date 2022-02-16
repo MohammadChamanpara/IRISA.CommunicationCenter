4e7c3f4b9377c676a532ea00d4b688971a7e8d99
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-02-16 14:29:15 +1100

    Add RemoveInProcessTelegrams tests

---
+++ IRISA.CommunicationCenter.Core.Tests/IccCoreTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using IRISA.CommunicationCenter.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace IRISA.CommunicationCenter.Core.Tests
+{
+    [TestClass]
+    public class IccCoreTests
+    {
+        [TestMethod]
+        public void RemoveInProcessTelegrams_Always_ShouldRemoveInProcessTelegrams()
+        {
+            //Arrange
+            var iccCore = new IccCore();
+            var telegrams = new List<IccTransfer>()
+            {
+                new IccTransfer() { ID=1 },
+                new IccTransfer() { ID=2 },
+                new IccTransfer() { ID=3 },
+                new IccTransfer() { ID=4 },
+                new IccTransfer() { ID=5 }
+            };
+
+            var inProcess = new HashSet<long>() { 1, 2 };
+
+            //Act
+            var sendingTelegrams = iccCore.RemoveInProcessTelegrams(telegrams, inProcess);
+
+            //Assert
+            sendingTelegrams.Count.Should().Be(3);
+            sendingTelegrams.TrueForAll(x => x.ID > 2);
+        }
+
+        [TestMethod]
+        public void RemoveInProcessTelegrams_WhenProcessingListIsEmpty_ShouldNotRemoveAnyTelegrams()
+        {
+            //Arrange
+            var iccCore = new IccCore();
+            var telegrams = new List<IccTransfer>()
+            {
+                new IccTransfer() { ID=1 },
+                new IccTransfer() { ID=2 },
+                new IccTransfer() { ID=3 },
+                new IccTransfer() { ID=4 },
+                new IccTransfer() { ID=5 }
+            };
+
+            var inProcess = new HashSet<long>();
+
+            //Act
+            var sendingTelegrams = iccCore.RemoveInProcessTelegrams(telegrams, inProcess);
+
+            //Assert
+            sendingTelegrams.Count.Should().Be(5);
+        }
+    }
+}
