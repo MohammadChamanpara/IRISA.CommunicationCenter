54214ec1d77297277a176e453553d91bfba8a715
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-02-21 10:31:30 +1100

    Add IccCore Unit Tests

--- IRISA.CommunicationCenter.Core.Tests/IccCoreTests.cs
+++ IRISA.CommunicationCenter.Core.Tests/IccCoreTests.cs
@@ -1,5 +1,5 @@
 ﻿using FluentAssertions;
-using IRISA.CommunicationCenter.Model;
+using IRISA.CommunicationCenter.Core.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,100 +10,10 @@ namespace IRISA.CommunicationCenter.Core.Tests
     public class IccCoreTests
     {
         [TestMethod]
-        public void RemoveInProcessTelegrams_Always_ShouldRemoveInProcessTelegrams()
-        {
-            //Arrange
-            var iccCore = new IccCore();
-            var telegrams = new List<IccTransfer>()
-            {
-                new IccTransfer() { ID=1 },
-                new IccTransfer() { ID=2 },
-                new IccTransfer() { ID=3 },
-                new IccTransfer() { ID=4 },
-                new IccTransfer() { ID=5 }
-            };
-
-            var inProcess = new HashSet<long>() { 1, 2 };
-
-            //Act
-            var sendingTelegrams = iccCore.RemoveInProcessTelegrams(telegrams, inProcess);
-
-            //Assert
-            sendingTelegrams.Count.Should().Be(3);
-            sendingTelegrams.TrueForAll(x => x.ID > 2);
-        }
-
-        [TestMethod]
-        public void RemoveInProcessTelegrams_WhenProcessingListIsEmpty_ShouldNotRemoveAnyTelegrams()
-        {
-            //Arrange
-            var iccCore = new IccCore();
-            var telegrams = new List<IccTransfer>()
-            {
-                new IccTransfer() { ID=1 },
-                new IccTransfer() { ID=2 },
-                new IccTransfer() { ID=3 },
-                new IccTransfer() { ID=4 },
-                new IccTransfer() { ID=5 }
-            };
-
-            var inProcess = new HashSet<long>();
-
-            //Act
-            var sendingTelegrams = iccCore.RemoveInProcessTelegrams(telegrams, inProcess);
-
-            //Assert
-            sendingTelegrams.Count.Should().Be(5);
-        }
-
-        [TestMethod]
-        public void AddTelegramsToProcessingList_WhenProcessingIsEmpty_ShouldAddAll()
-        {
-            //Arrange
-            var iccCore = new IccCore();
-            var telegrams = new List<IccTransfer>()
-            {
-                new IccTransfer() { ID=1 },
-                new IccTransfer() { ID=2 },
-                new IccTransfer() { ID=3 }
-            };
-
-            var inProcess = new HashSet<long>();
-
-            //Act
-            var newInprocess = iccCore.AddTelegramsToProcessingList(telegrams, inProcess);
-
-            //Assert
-            newInprocess.Count.Should().Be(3);
-        }
-
-        [TestMethod]
-        public void AddTelegramsToProcessingList_WhenAlreayInProcessing_ShouldNotAdd()
-        {
-            //Arrange
-            var iccCore = new IccCore();
-            var telegrams = new List<IccTransfer>()
-            {
-                new IccTransfer() { ID=1 },
-                new IccTransfer() { ID=2 },
-                new IccTransfer() { ID=3 }
-            };
-
-            var inProcess = new HashSet<long>() { 1, 2 };
-
-            //Act
-            var newInprocess = iccCore.AddTelegramsToProcessingList(telegrams, inProcess);
-
-            //Assert
-            newInprocess.Count.Should().Be(3);
-            newInprocess.Should().BeEquivalentTo(new long[] { 1, 2, 3 });
-        }
-
-        [TestMethod]
         public void GroupTelegramsByDestination_Always_ShouldGroup()
         {
             //Arrange
-            var iccCore = new IccCore();
+            var iccCore = new IccCore(new InProcessTelegrams());
             var telegrams = new List<IccTransfer>()
             {
                 new IccTransfer() { ID=1, DESTINATION="1" },
@@ -122,7 +32,7 @@ namespace IRISA.CommunicationCenter.Core.Tests
             groupedTelegrams.Count.Should().Be(3);
 
             groupedTelegrams.First().Count.Should().Be(3);
-            groupedTelegrams.First().All(x=>x.DESTINATION=="1").Should().BeTrue();
+            groupedTelegrams.First().All(x => x.DESTINATION == "1").Should().BeTrue();
 
             groupedTelegrams[1].Count.Should().Be(2);
             groupedTelegrams[1].All(x => x.DESTINATION == "2").Should().BeTrue();
---
+++ IRISA.CommunicationCenter.Core.Tests/InProcessTelegramsTests.cs
@@ -0,0 +1,98 @@
+﻿using FluentAssertions;
+using IRISA.CommunicationCenter.Core.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace IRISA.CommunicationCenter.Core.Tests
+{
+    [TestClass]
+    public class InProcessTelegramsTests
+    {
+        [TestMethod]
+        public void RemoveFrom_Always_ShouldRemoveInProcessTelegrams()
+        {
+            //Arrange
+            var inProcessTelegrams = new InProcessTelegrams(1,2);
+            var telegrams = new List<IccTransfer>()
+            {
+                new IccTransfer() { ID=1 },
+                new IccTransfer() { ID=2 },
+                new IccTransfer() { ID=3 },
+                new IccTransfer() { ID=4 },
+                new IccTransfer() { ID=5 }
+            };
+
+            //Act
+            var sendingTelegrams = inProcessTelegrams.RemoveFrom(telegrams);
+
+            //Assert
+            sendingTelegrams.Count.Should().Be(3);
+            sendingTelegrams.TrueForAll(x => x.ID > 2);
+        }
+
+        [TestMethod]
+        public void RemoveFrom_WhenProcessingListIsEmpty_ShouldNotRemoveAnyTelegrams()
+        {
+            //Arrange
+            var inProcessTelegrams = new InProcessTelegrams();
+
+            var telegrams = new List<IccTransfer>()
+            {
+                new IccTransfer() { ID=1 },
+                new IccTransfer() { ID=2 },
+                new IccTransfer() { ID=3 },
+                new IccTransfer() { ID=4 },
+                new IccTransfer() { ID=5 }
+            };
+
+            //Act
+            var sendingTelegrams = inProcessTelegrams.RemoveFrom(telegrams);
+
+            //Assert
+            sendingTelegrams.Count.Should().Be(5);
+        }
+
+        [TestMethod]
+        public void AddRange_WhenProcessingIsEmpty_ShouldAddAll()
+        {
+            //Arrange
+            var inProcessTelegrams = new InProcessTelegrams();
+
+            var telegrams = new List<IccTransfer>()
+            {
+                new IccTransfer() { ID=1 },
+                new IccTransfer() { ID=2 },
+                new IccTransfer() { ID=3 }
+            };
+
+            //Act
+            inProcessTelegrams.AddRange(telegrams);
+
+            //Assert
+            inProcessTelegrams.GetAllIds().Count.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void AddRange_WhenAlreayInProcessing_ShouldNotAdd()
+        {
+            //Arrange
+            var inProcessTelegrams = new InProcessTelegrams(1,2);
+
+            var telegrams = new List<IccTransfer>()
+            {
+                new IccTransfer() { ID=1 },
+                new IccTransfer() { ID=2 },
+                new IccTransfer() { ID=3 }
+            };
+
+            //Act
+            inProcessTelegrams.AddRange(telegrams);
+
+            //Assert
+            inProcessTelegrams.GetAllIds().Count.Should().Be(3);
+            inProcessTelegrams.GetAllIds().Should().BeEquivalentTo(new long[] { 1, 2, 3 });
+        }
+
+        
+    }
+}
