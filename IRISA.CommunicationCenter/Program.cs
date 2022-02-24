e645c807a63971bfa6e00616cd4d4d85ab56761b
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-02-24 14:47:37 +1100

    Use Dependency Injection in program

--- IRISA.CommunicationCenter/Program.cs
+++ IRISA.CommunicationCenter/Program.cs
@@ -1,41 +1,59 @@
+using IRISA.CommunicationCenter.Core;
 using IRISA.CommunicationCenter.Forms;
 using IRISA.CommunicationCenter.Library.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading;
 using System.Windows.Forms;
+
 namespace IRISA.CommunicationCenter
 {
     internal static class Program
-	{
-		[STAThread]
-		private static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
-			Application.ThreadException += new ThreadExceptionEventHandler(Program.Application_ThreadException);
-			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Program.CurrentDomain_UnhandledException);
-            Mutex mutex = new Mutex(true, "Irisa.CommunicationCenter", out bool firstInstanceOfApp);
+    {
+        public static IServiceProvider ServiceProvider { get; set; }
+
+        [STAThread]
+        private static void Main()
+        {
+            _ = new Mutex(true, "Irisa.CommunicationCenter", out bool firstInstanceOfApp);
             if (!firstInstanceOfApp)
-			{
-				MessageForm.ShowErrorMessage("نسخه دیگری از نرم افزار مرکز ارتباطات ایریسا ( {0} ) در حال اجرا می باشد", new object[]
-				{
-					"ICC"
-				});
-			}
-			else
-			{
-				Application.Run(new MainForm());
-			}
-		}
-		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-		{
-			Exception exception = (Exception)e.ExceptionObject;
-			new LoggerInMemory().LogException(exception, "بروز خطای کنترل نشده.");
-		}
-		private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
-		{
-			new LoggerInMemory().LogException(e.Exception, "بروز خطای کنترل نشده.");
-		}
-	}
+            {
+                MessageForm.ShowErrorMessage("نسخه دیگری از نرم افزار مرکز ارتباطات ایریسا در حال اجرا می باشد");
+                return;
+            }
+
+            InitializeApplication();
+
+            ConfigureServices();
+
+            Application.Run(new MainForm());
+        }
+
+        private static void InitializeApplication()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IIccQueue, IccQueueInMemory>();
+            services.AddSingleton<ILogger, LoggerInMemory>();
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ServiceProvider.GetService<ILogger>().LogException((Exception)e.ExceptionObject, "بروز خطای کنترل نشده.");
+        }
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            ServiceProvider.GetService<ILogger>().LogException(e.Exception, "بروز خطای کنترل نشده.");
+
+        }
+    }
 }
