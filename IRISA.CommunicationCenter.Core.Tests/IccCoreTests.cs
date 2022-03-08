19ba270fde862c563befdb11918c8a15e016dc5d
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-03-08 13:13:17 +1100

    Install Moq Nuget Package

---
+++ IRISA.CommunicationCenter.Core.Tests/IccCoreTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IRISA.CommunicationCenter.Core.Tests
+{
+    [TestClass]
+    public class IccCoreTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var x=new IccCore()
+        }
+    }
+}
